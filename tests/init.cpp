c82f16d75e921913cedf8e407ad4c773c4c10ce4
yanaxgrishkova <yanaxgrishkova@gmail.com>
2017-03-19 21:02:25 +0300

    Update init.cpp

--- tests/init.cpp
+++ tests/init.cpp
@@ -1,8 +1,14 @@
 #include <complex_t.hpp>
 #include <catch.hpp>
 
-SCENARIO("complex_t init", "[init]") {
+SCENARIO("complex_t", "default constructor") {
 	complex_t complex;
 	REQUIRE(complex.real_() == 0);
 	REQUIRE(complex.imaginary_() == 0);
+}
+
+SCENARIO("complex_t", "constructor with params") {
+	complex_t complex(1, 2);
+	REQUIRE(complex.real_() == 1);
+	REQUIRE(complex.imaginary_() == 2);
 }
